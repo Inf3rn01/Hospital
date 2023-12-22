6df42b86f418df1d2b16d4edbc458687b00438b7
Danil <danilpugachev253@gmail.com>
2023-12-23 02:00:57 +0300

    Изменена архитектура проекта

--- src/server/database/errors.py
+++
@@ -1,3 +0,0 @@
-class MissedScript(Exception):
-    def __str__(self):
-        return 'One or more sql scripts are missing.'
--- src/server/settings.py
+++ src/server/settings.py
@@ -1,7 +1,7 @@
 # Database settings
-DB_PATH = "../Hospital/src/server/database/hospital.db"
+DB_PATH = "C:/Hospital/src/server/database/hospital.db"
 
-CREATE = '../Hospital/src/server/database/sql_scripts/create.sql'
+CREATE = 'C:/Hospital/src/server/database/sql_scripts/create.sql'
 FILL = 'C:/Hospital/src/server/database/sql_scripts/fill.sql'
 
 # Server settings
